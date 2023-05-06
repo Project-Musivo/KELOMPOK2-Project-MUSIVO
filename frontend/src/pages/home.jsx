21412e6b8a11c59c73102e764e6a78ef0f748531
iqblfrdsyh <iqblfrdsyh@gmail.com>
2023-05-06 18:26:08 +0700

    build react vite

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Halo Dunia</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div className="home">
-      <h1>Halo Dunia</h1>
-    </div>
-  );
-};
-
-export default Home;
