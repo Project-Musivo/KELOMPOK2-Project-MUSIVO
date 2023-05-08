3d523c3cc7b49b2060f9a810a34b1f02ba40b21d
Widya <lalunadelavonte@gmail.com>
2023-05-08 11:53:00 +0700

    menambahkan asset dan beberapa img, membuat sidebar done by widya

---
+++ frontend/src/components/layouts/sidebar/sidebar.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import Logo from "../../../assets/logo.png";
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+  const Menus = [
+    { title: "Home", src: "home" },
+    { title: "Playlist", src: "playlist" },
+    { title: "Artisc ", src: "user" },
+    { title: "Favorit", src: "favorit" },
+    { title: "Leaderboard", src: "trophy" },
+    { title: "Settings ", src: "setting"},
+    { title: "Logout", src: "logout", gap: true},
+  ];
+
+  return (
+    <div className="flex">
+      <div
+        className={` ${
+          open ? "w-72" : "w-20 "
+        } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
+      >
+        <img
+          src="./src/assets/control.png"
+          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+           border-2 rounded-full  ${!open && "rotate-180"}`}
+          onClick={() => setOpen(!open)}
+        />
+        <div className="flex gap-x-4 items-center">
+          <img
+            src={Logo}
+            className={`w-10 cursor-pointer duration-500 ${
+              open && "rotate-[360deg]"
+            }`}
+          />
+          <h1
+            className={`text-white origin-left font-medium text-xl duration-200 ${
+              !open && "scale-0"
+            }`}
+          >
+            Musivo
+          </h1>
+        </div>
+        <ul className="pt-6">
+          {Menus.map((Menu, index) => (
+            <li
+              key={index}
+              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+              ${Menu.gap ? "mt-9" : "mt-2"} ${
+                index === 0 && "bg-light-white"
+              } `}
+            >
+              <img src={`./src/assets/${Menu.src}.png`} />
+              <span className={`${!open && "hidden"} origin-left duration-200`}>
+                {Menu.title}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,23 +2,30 @@ import React from "react";
 import NavigationBar from "../components/layouts/navigationBar.jsx/navigationBar";
 import Genre from "../components/genreMusic/genre";
 import MostlyPlayed from "../components/mostly played/mostPlay";
+import Sidebar from "../components/layouts/sidebar/sidebar";
+
 
 const Home = () => {
   return (
     <React.Fragment>
-      <NavigationBar />
-      <div className="home ps-[120px]">
-        <div className="ms-[30px]">
-          <div className="ucapan mt-[40px]">
-            <h3 className="text-[28px] font-semibold">Good Morning, Nanaa</h3>
+      <div className="flex">
+        <Sidebar/>
+        <div>
+          <NavigationBar />
+          <div className="home ps-[120px]">
+            <div className="ms-[30px]">
+              <div className="ucapan mt-[40px]">
+                <h3 className="text-[28px] font-semibold">Good Morning, Nanaa</h3>
+              </div>
+              <Genre />
+            </div>
+            <div className="mostPlay">
+              <h2 className="text-[25px] mt-[30px] mb-3 font-semibold ms-3">
+                Mostly Played
+              </h2>
+              <MostlyPlayed />
+            </div>
           </div>
-          <Genre />
-        </div>
-        <div className="mostPlay">
-          <h2 className="text-[25px] mt-[30px] mb-3 font-semibold ms-3">
-            Mostly Played
-          </h2>
-          <MostlyPlayed />
         </div>
       </div>
     </React.Fragment>
