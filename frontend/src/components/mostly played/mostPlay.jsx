b493a1897e86772609903f4ca399d4465f2f1b9c
iqblfrdsyh <iqblfrdsyh@gmail.com>
2023-05-08 08:33:19 +0700

    menambahkan genre, most play, navbar

---
+++ frontend/src/components/mostly played/mostPlay.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const MostlyPlayed = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const togglePlay = (i) => {
+    setActiveIndex(i === activeIndex ? null : i);
+  };
+
+  const boxes = [];
+  for (let i = 1; i <= 6; i++) {
+    const isPlaying = activeIndex === i;
+    boxes.push(
+      <div
+        className="boxPlay bg-white text-black py-3 w-[325px] flex items-center justify-evenly flex-wrap rounded-[5px]"
+        key={i}
+      >
+        <span className="font-extrabold">0{i}</span>
+        <img
+          src="./assets/album music/anggurman.png"
+          alt=""
+          className="w-[80px]"
+        />
+        <div className="title" style={{ marginLeft: "-10px" }}>
+          <h3>Anggurman</h3>
+          <p className="text-[15px] opacity-50">Jason Renti</p>
+        </div>
+
+        <img
+          src={
+            isPlaying ? "./assets/icon/pause.png" : "./assets/icon/start.png"
+          }
+          alt=""
+          className="cursor-pointer"
+          onClick={() => togglePlay(i)}
+        />
+        <img src="./assets/icon/detail.png" alt="" className="cursor-pointer" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="mostlyplayed flex justify-start gap-3 flex-wrap w-[60%]">
+      {boxes}
+    </div>
+  );
+};
+
+export default MostlyPlayed;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,10 +1,27 @@
-import "../index.css";
+import React from "react";
+import NavigationBar from "../components/layouts/navigationBar.jsx/navigationBar";
+import Genre from "../components/genreMusic/genre";
+import MostlyPlayed from "../components/mostly played/mostPlay";
 
 const Home = () => {
   return (
-    <div className="home text-yellow-400">
-      <h1 className="text-yellow-500">Halo Dunia</h1>
-    </div>
+    <React.Fragment>
+      <NavigationBar />
+      <div className="home ps-[120px]">
+        <div className="ms-[30px]">
+          <div className="ucapan mt-[40px]">
+            <h3 className="text-[28px] font-semibold">Good Morning, Nanaa</h3>
+          </div>
+          <Genre />
+        </div>
+        <div className="mostPlay">
+          <h2 className="text-[25px] mt-[30px] mb-3 font-semibold ms-3">
+            Mostly Played
+          </h2>
+          <MostlyPlayed />
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
