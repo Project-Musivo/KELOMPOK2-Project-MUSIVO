070da0e98000750503afd2953a4cae6d82c862e9
RullyLK <110457904+RullyLK@users.noreply.github.com>
2023-05-08 17:29:30 +0700

    Login page - Rully

---
+++ frontend/src/pages/LogReg/register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContainerLogReg from "../../components/containerLogReg";
+
+export default function Login() {
+  return (
+    <div>
+      <ContainerLogReg title='Login' buttonText='Login' />
+      <div>
+      </div>
+    </div>
+  );
+}
