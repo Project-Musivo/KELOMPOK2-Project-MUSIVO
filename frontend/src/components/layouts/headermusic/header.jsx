77173a635e504141c9be694d2ac3bcfa274569ee
WidyaPuAs <edithcasean@gmail.com>
2023-05-14 17:18:57 +0700

    perubahan page favorit by widya dan menambahkan listfav

--- frontend/src/components/layouts/headermusic/header.jsx
+++ frontend/src/components/layouts/headermusic/header.jsx
@@ -1,11 +1,11 @@
-// import React, { useState} from "react";
+import React, { useState} from "react";
 
-// const Headermusic = () => {
-//     return (
-//         <div className=" bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0">
-            
-//         </div>
-//     );
-// };
+const Headermusic = () => {
+    return (
+        <div className="bg-white">
+            <h1>test</h1>
+        </div>
+    );
+};
 
-// export default Headermusic;
+export default Headermusic;
--- frontend/src/pages/favorit.jsx
+++ frontend/src/pages/favorit.jsx
@@ -1,16 +1,58 @@
-// import { useState } from "react";
-import Sidebar from "../components/layouts/sidebar/sidebar";
+import React, { useState } from "react";
 import NavigationBar from "../components/layouts/navigationBar/navigationBar";
+import Sidebar from "../components/layouts/sidebar/sidebar";
+import Listfav from "../components/listFav/listfav";
+// import TableList from "../components/tableList/tableList";
 
-const Favorit = () => {
-  return (
-    <div className="flex">
-      <Sidebar />
-      <div className="w-full">
-        <NavigationBar />
-        <h1>Test fav</h1>
+  const Favorit = () => {
+    const [isToogle, setIsToogle] = useState(false);
+    const toogleClick = () => {
+      setIsToogle(!isToogle);
+    };
+    return (
+    <React.Fragment>
+      <div className="flex">
+        <Sidebar />
+        <div className="w-screen">
+          <NavigationBar />
+          <div className="playlist ps-[125px] pe-[50px]">
+            <div className="flex mt-[100px] mb-[50px]">
+              <img
+                src="./assets/album music/collide.jpg"
+                className="w-[225px] rounded-lg"
+              />
+              <div className="flex flex-col mt-[80px] mx-10">
+                <h1 className="text-[28px] font-bold ">Morning Vibes</h1>
+                <p className="text-[12px]">
+                  <span className="font-bold">Nanaa</span>, 4 Playlist , 6 Mei
+                  2023
+                </p>
+                <div className="flex">
+                  <img
+                    src={
+                      isToogle
+                        ? "./assets/icon/paused.png"
+                        : "./assets/icon/play.png"
+                    }
+                    onClick={toogleClick}
+                    alt=""
+                    className="cursor-pointer w-[50px]  mt-[25px]"
+                  />
+                  <img
+                    src="./assets/icon/detail-white.png"
+                    alt=""
+                    className="cursor-pointer w-[35px] h-[10px] mx-8 mt-[44px]"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <Listfav />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 export default Favorit;
