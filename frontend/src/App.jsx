5ecf70db672d090ca1698cd717fbf7a92f53ac2b
BintangLuthfian <wortelmobile@gmail.com>
2023-06-12 20:19:20 +0700

    Penambahan Admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dasboardadmin from "./pages/dasboardadmin";
-import Music from "./pages/musicadmin";
-import DataUser from "./pages/datauseradmin";
-import Setting from "./pages/settingadmin";
+import Home from "./pages/home";
+import Favorit from "./pages/favorit";
+import Playlist from "./pages/playlist";
+import ProfileSetting from "./pages/settingPage/profilesetting";
+import Login from "./pages/LogReg/Login";
+// import Regis from "./pages/LogReg/Regis";
 
+import Register from "./pages/LogReg/register";
+
+import About from "./pages/settingPage/about";
+
+import Leaderboard from "./pages/leaderboard";
+import Premium from "./pages/premium";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dasboardadmin />} />
-        <Route path="/Music" element={<Music />} />
-        <Route path="/DataUser" element={<DataUser />} />
-        <Route path="/Setting" element={<Setting />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/regis" element={<Regis />} /> */}
+        <Route path="/favorit" element={<Favorit />} />
+        <Route path="/profilesetting" element={<ProfileSetting />} />
+        <Route path="/playlist" element={<Playlist />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/premium" element={<Premium />} />
       </Routes>
     </BrowserRouter>
   );
