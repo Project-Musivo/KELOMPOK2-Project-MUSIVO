edb71b8c0d4cf0abe997f4269b768e48a4bde4a0
BintangLuthfian <wortelmobile@gmail.com>
2023-06-12 20:26:18 +0700

    File Admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,3 +33,27 @@ function App() {
 }
 
 export default App;
+
+// Untuk Admin
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Dasboardadmin from "./pages/dasboardadmin";
+// import Music from "./pages/musicadmin";
+// import DataUser from "./pages/datauseradmin";
+// import Setting from "./pages/settingadmin";
+
+
+// function App() {
+  // return (
+    // <BrowserRouter>
+      // <Routes>
+        // <Route path="/" element={<Dasboardadmin />} />
+        // <Route path="/Music" element={<Music />} />
+        // <Route path="/DataUser" element={<DataUser />} />
+        // <Route path="/Setting" element={<Setting />} />
+      // </Routes>
+    // </BrowserRouter>
+  // );
+// }
+
+// export default App;
