2c161bd86ce869262f82e21ef0349957d45459d0
drjtyudo <drajatyudo3@gmail.com>
2023-06-12 16:19:22 +0700

    menambahkan page untuk premium

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,15 @@ import Home from "./pages/home";
 import Favorit from "./pages/favorit";
 import Playlist from "./pages/playlist";
 import ProfileSetting from "./pages/settingPage/profilesetting";
-
 import Login from "./pages/LogReg/Login";
-import Regis from "./pages/LogReg/Regis";
+// import Regis from "./pages/LogReg/Regis";
 
-// import Register from "./pages/LogReg/register";
+import Register from "./pages/LogReg/register";
 
 import About from "./pages/settingPage/about";
 
 import Leaderboard from "./pages/leaderboard";
+import Premium from "./pages/premium";
 
 function App() {
   return (
@@ -20,12 +20,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/regis" element={<Regis />} />
+        {/* <Route path="/regis" element={<Regis />} /> */}
         <Route path="/favorit" element={<Favorit />} />
         <Route path="/profilesetting" element={<ProfileSetting />} />
         <Route path="/playlist" element={<Playlist />} />
         <Route path="/about" element={<About />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/premium" element={<Premium />} />
       </Routes>
     </BrowserRouter>
   );
