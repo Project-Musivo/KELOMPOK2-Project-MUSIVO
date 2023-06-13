0a8e4c0f897af7195c9138d652f779ac6e06a4b8
BintangLuthfian <Wortelmobile@gmail.com>
2023-06-14 00:57:31 +0700

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,26 +30,27 @@ function App() {
 
 export default App;
 
-// Untuk Admin
 
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Dasboardadmin from "./pages/dasboardadmin";
-// import Music from "./pages/musicadmin";
-// import DataUser from "./pages/datauseradmin";
-// import Setting from "./pages/settingadmin";
+//admin
+
+ // import { BrowserRouter, Routes, Route } from "react-router-dom";
+ // import Dasboardadmin from "./admin/dasboardadmin";
+ // import Music from "./admin/musicadmin";
+ // import DataUser from "./admin/datauseradmin";
+ // import Setting from "./admin/settingadmin";
 
 
-// function App() {
-  // return (
-    // <BrowserRouter>
+ // function App() {
+   // return (
+     // <BrowserRouter>
       // <Routes>
-        // <Route path="/" element={<Dasboardadmin />} />
-        // <Route path="/Music" element={<Music />} />
-        // <Route path="/DataUser" element={<DataUser />} />
-        // <Route path="/Setting" element={<Setting />} />
-      // </Routes>
-    // </BrowserRouter>
-  // );
-// }
+         // <Route path="/" element={<Dasboardadmin />} />
+       // <Route path="/Music" element={<Music />} />
+         // <Route path="/DataUser" element={<DataUser />} />
+         // <Route path="/Setting" element={<Setting />} />
+       // </Routes>
+     // </BrowserRouter>
+   // );
+ // }
 
-// export default App;
+ // export default App;
