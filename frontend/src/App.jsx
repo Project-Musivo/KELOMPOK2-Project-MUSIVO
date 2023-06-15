bf006dbff311e488b98fd90719c1ddd9675cd9b2
iqblfrdsyh <iqblfrdsyh@gmail.com>
2023-06-15 07:26:20 +0700

    merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Favorit from "./pages/favorit";
 import Playlist from "./pages/playlist";
 import Login from "./pages/LogReg/Login";
 import Regis from "./pages/LogReg/Regis";
-import ProfileSetting from "./pages/settingPage/profilesetting";
+// import ProfileSetting from "./pages/settingPage/profilesetting";
 
 import About from "./pages/settingPage/about";
 import Leaderboard from "./pages/leaderboard";
@@ -32,27 +32,25 @@ function App() {
 
 export default App;
 
-
 //admin
 
- // import { BrowserRouter, Routes, Route } from "react-router-dom";
- // import Dasboardadmin from "./admin/dasboardadmin";
- // import Music from "./admin/musicadmin";
- // import DataUser from "./admin/datauseradmin";
- // import Setting from "./admin/settingadmin";
-
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Dasboardadmin from "./admin/dasboardadmin";
+// import Music from "./admin/musicadmin";
+// import DataUser from "./admin/datauseradmin";
+// import Setting from "./admin/settingadmin";
 
 // function App() {
-  // return (
-    // <BrowserRouter>
-      // <Routes>
-        // <Route path="/" element={<Dasboardadmin />} />
-        // <Route path="/Music" element={<Music />} />
-        // <Route path="/DataUser" element={<DataUser />} />
-        // <Route path="/Setting" element={<Setting />} />
-      // </Routes>
-    // </BrowserRouter>
-  // );
+// return (
+// <BrowserRouter>
+// <Routes>
+// <Route path="/" element={<Dasboardadmin />} />
+// <Route path="/Music" element={<Music />} />
+// <Route path="/DataUser" element={<DataUser />} />
+// <Route path="/Setting" element={<Setting />} />
+// </Routes>
+// </BrowserRouter>
+// );
 // }
 
- // export default App;
+// export default App;
