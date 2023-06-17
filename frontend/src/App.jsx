7762677492f0c58ff7941a03e53a3a6e3e3dc961
Fachriiii Firdaus <fachri4606@gmail.com>
2023-06-18 06:39:49 +0700

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,46 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Favorit from "./pages/favorit";
 import Playlist from "./pages/playlist";
-
 import Login from "./pages/LogReg/Login";
 import Regis from "./pages/LogReg/Regis";
-
-
-import Login from "./pages/LogReg/register";
-
 import AboutDev from "./pages/settingPage/about";
-
 import Leaderboard from "./pages/leaderboard";
-
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/favorit" element={<Favorit />} />
-         <Route path="/playlist" element={<Playlist />} />
-
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-
+        <Route path="/playlist" element={<Playlist />} />
         <Route path="/regis" element={<Regis />} />
-
-
         <Route path="/favorit" element={<Favorit />} />
         <Route path="/playlist" element={<Playlist />} />
-
-
-
-
         <Route path="/about" element={<AboutDev />} />
-
-
         <Route path="/leaderboard" element={<Leaderboard />} />
-
       </Routes>
     </BrowserRouter>
   );
