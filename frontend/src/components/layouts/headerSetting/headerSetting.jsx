c4be8d71a5ac8de5f23c46787a8d6b5ff06718cf
WidyaPuAs <edithcasean@gmail.com>
2023-06-24 19:36:16 +0700

    reset

--- frontend/src/components/layouts/headerSetting/headerSetting.jsx
+++
@@ -1,9 +0,0 @@
-const HeaderSetting = (props) => {
-  return (
-    <div className="border-b-2 h-[80px] w-full top-[68px] ps-[60px] pt-[15px] fixed mb-[100px] bg-[#141414]">
-      <h2 className="text-[35px] font-semibold">{props.title}</h2>
-    </div>
-  );
-};
-
-export default HeaderSetting;
