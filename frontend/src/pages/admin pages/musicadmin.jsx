b7ab1f5dd662c902e72b00b8674eec6406057fe4
Sidiq Pramono <117287328+sidiqprmn@users.noreply.github.com>
2023-08-26 19:30:09 +0700

    Update musicadmin.jsx

--- frontend/src/pages/admin pages/musicadmin.jsx
+++ frontend/src/pages/admin pages/musicadmin.jsx
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 import Sidebaradmin from "../../components/layouts/sidebar/sidebaradmin";
 import Navbar from "../../admin components/navbarmusic";
 import TableMusic from "../../admin components/tablemusic";
-<<<<<<< HEAD
-=======
 
->>>>>>> Narendi
 const Music = () => {
   return (
     <React.Fragment>
